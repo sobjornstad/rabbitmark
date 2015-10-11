b13de3952cb58cb45ab7bb46ca11d056de08d7b6
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2015-10-11 17:51:51 -0500

    allow editing things
    Unfortunately, I've run into the age-old commit-is-slow problem -- but
    it's absolutely necessary to commit after a delete in order to get
    things to work (because SQLAlchemy, I suppose). The tags go haywire
    otherwise. I don't think it's that big of a deal, but it is kind of
    irritating. I also saved a substantial amount of time, for whatever
    reason, or at least my perception is that I did, by iterating over long
    lists of tags twice to update the tag list rather than just clearing it
    and adding it again. Maybe addItem() is slower than I thought.
    
    Also, things work now! Yay! Now we just have to be able to add items and
    this can begin to be somewhat useful.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
-from sqlalchemy import create_engine, and_, or_
+from sqlalchemy import create_engine, event, and_, or_
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -94,11 +95,42 @@ class BookmarkTableModel(QAbstractTableModel):
         self.endResetModel()
 
     def saveIfEdited(self, mark, content):
-        #currentData = self.L[index.row()]
-        print "checkEdited dump"
-        print mark
-        print content
+        """
+        If the content in 'content' differs from the data in the db obj
+        'mark', update 'mark' to match the contents of 'content'.
 
+        Returns True if an update was made, False if not.
+        """
+        #currentData = self.L[index.row()]
+        if not (mark.name == content['name'] and
+                mark.description == content['descr'] and
+                mark.url == content['url'] and
+                [i.text for i in mark.tags_rel] == content['tags']):
+            print "need save!"
+            mark.name = content['name']
+            mark.description = content['descr']
+            mark.url = content['url']
+
+            # add new tags
+            new_tags = content['tags']
+            for tag in new_tags:
+                existingTag = self.session.query(Tag).filter(
+                              Tag.text == tag).first()
+                if existingTag:
+                    mark.tags_rel.append(existingTag)
+                else:
+                    new_tag = Tag(text=tag)
+                    self.session.add(new_tag)
+                    mark.tags_rel.append(new_tag)
+            # remove tags that are no longer used
+            deletedDirty = False
+            for tag in mark.tags_rel:
+                if tag.text not in new_tags:
+                    deletedDirty = True
+                    self.session.delete(tag)
+            self.session.commit()
+            return True
+        return False
 
     def getObj(self, index):
         try:
@@ -107,14 +139,13 @@ class BookmarkTableModel(QAbstractTableModel):
             return None
 
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.form = Ui_MainWindow()
         self.form.setupUi(self)
 
-        Session = make_Session()
+        self.Session = make_Session()
 
         # set up actions
         self.form.action_Quit.triggered.connect(self.quit)
@@ -125,15 +156,16 @@ class MainWindow(QMainWindow):
 
         # set up data table
         self.tableView = self.form.bookmarkTable
-        self.tableModel = BookmarkTableModel(self, Session)
+        self.tableModel = BookmarkTableModel(self, self.Session)
         self.tableView.setModel(self.tableModel)
         self.sm = self.tableView.selectionModel()
         self.sm.selectionChanged.connect(self.fillEditPane)
 
         # set up tag list
-        self.tags = scan_tags(Session)
+        self.tags = scan_tags(self.Session)
         for i in self.tags:
             self.form.tagList.addItem(i)
+        self.form.tagList.sortItems()
 
         # set up re-search triggers and update for the first time
         self.form.searchBox.textChanged.connect(self.doUpdateForSearch)
@@ -147,6 +179,31 @@ class MainWindow(QMainWindow):
     def quit(self):
         # commit? we don't have a session in here
         sys.exit(0)
+
+    def resetTagList(self):
+        self.tags = scan_tags(self.Session)
+        for i in range(self.form.tagList.count()):
+            if self.form.tagList.item(i).text() not in self.tags:
+                self.form.tagList.takeItem(i)
+        for i in self.tags:
+            if not self.form.tagList.findItems(i, Qt.MatchExactly):
+                self.form.tagList.addItem(i)
+        self.form.tagList.sortItems()
+
+    def maybeSaveBookmark(self, old, new):
+        """
+        If focus changed away from one of the editable boxes, update state of
+        its associated db object.
+
+        Called by signal set in startQt() when any focus changes.
+        """
+        sf = self.form
+        if old in (sf.nameBox, sf.urlBox, sf.descriptionBox, sf.tagsBox):
+            mark = self.tableModel.getObj(self.tableView.currentIndex())
+            QApplication.processEvents()
+            if self.tableModel.saveIfEdited(mark, self.mRepr()):
+                self.resetTagList()
+
 
     def doUpdateForSearch(self):
         """
@@ -205,19 +262,18 @@ class MainWindow(QMainWindow):
         Short for "mark representation": return a dictionary of the content
         currently in the fields so that the model can compare and/or save it.
         """
-        print "mRepr()"
         return {
                 'name':  unicode(self.form.nameBox.text()),
                 'url':   unicode(self.form.urlBox.text()),
                 'descr': unicode(self.form.descriptionBox.toPlainText()),
                 'tags':  [i.strip() for i in
-                          unicode(self.form.tagsBox.text()).split(',')],
+                          unicode(self.form.tagsBox.text()).split(',')
+                          if i.strip() != ''],
                }
 
 def scan_tags(Session):
     session = Session()
     tag_list = [unicode(i) for i in session.query(Tag).all()]
-    tag_list.sort()
     return tag_list
 
 def make_Session():
@@ -226,7 +282,13 @@ def make_Session():
     Base.metadata.create_all(engine) # will not recreate existing db's
     return Session
 
-
+# http://stackoverflow.com/questions/9671490/
+# how-to-set-sqlite-pragma-statements-with-sqlalchemy
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.close()
 
 
 def dbTest():
@@ -239,6 +301,7 @@ def dbTest():
         print "1) Add a bookmark"
         print "2) Search for a bookmark"
         print "3) Delete a bookmark"
+        print "4) Test tag editing"
         print "0) Quit"
         what_do = raw_input("> ")
 
@@ -269,7 +332,7 @@ def dbTest():
                     Bookmark.name.like(search_for)):
                 print "Name: %s" % bookmark.name
                 print "URL : %s" % bookmark.url
-                print "Tags: %s" % bookmark.tags
+                print "Tags: %r" % [i.text for i in bookmark.tags_rel]
                 print "Description:"
                 print bookmark.description
                 print ""
@@ -286,6 +349,35 @@ def dbTest():
                     break
                 session.delete(bookmark)
 
+        elif what_do == "4":
+            print 'Adjusting tags for "Lillian".'
+            mark = session.query(Bookmark).filter(Bookmark.name == 'Lillian').one()
+            new_tags_raw = raw_input("New tags: ")
+            new_tags = [i.strip() for i in new_tags_raw.split(',')]
+
+            for tag in new_tags:
+                existingTag = session.query(Tag).filter(Tag.text == tag).first()
+                if existingTag:
+                    mark.tags_rel.append(existingTag)
+                else:
+                    new_tag = Tag(text=tag)
+                    mark.tags_rel.append(new_tag)
+            for tag in mark.tags_rel:
+                if tag.text not in new_tags:
+                    session.delete(tag)
+
+
+            #for tag in new_tags:
+                #existingTag = session.query(Tag).filter(Tag.text == tag).first()
+                ##if not existingTag:
+                    #session
+
+
+
+            for tag in mark.tags_rel:
+                print tag.text
+
+
         elif what_do == "0":
             print "Exiting."
             session.commit()
@@ -296,6 +388,7 @@ def dbTest():
 def startQt():
     app = QApplication(sys.argv)
     mw = MainWindow()
+    app.focusChanged.connect(mw.maybeSaveBookmark)
     mw.show()
     app.exec_()
 
