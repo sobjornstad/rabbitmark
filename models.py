181ac943ee7c930344ea25c22654f7dc4c569383
Soren I. Bjornstad <contact@sorenbjornstad.com>
2019-06-18 06:39:02 -0500

    clean up models file

--- models.py
+++ models.py
@@ -1,16 +1,28 @@
+# This file is part of RabbitMark.
+# Copyright 2015, 2019 Soren Bjornstad. All rights reserved.
+
+"""
+models.py - SQLAlchemy ORM database model for RabbitMark
+"""
+
+#pylint: disable=invalid-name
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy import Integer, String, Boolean, \
         Table, Column, ForeignKey, PrimaryKeyConstraint
 
 Base = declarative_base()
+mark_tag_assoc = Table(
+    'mark_tag_assoc',
+    Base.metadata,
+    Column('mark_id', Integer, ForeignKey('bookmarks.id')),
+    Column('tag_id', Integer, ForeignKey('tags.id')),
+    PrimaryKeyConstraint('mark_id', 'tag_id'))
 
-mark_tag_assoc = Table('mark_tag_assoc', Base.metadata,
-        Column('mark_id', Integer, ForeignKey('bookmarks.id')),
-        Column('tag_id', Integer, ForeignKey('tags.id')),
-        PrimaryKeyConstraint('mark_id', 'tag_id'))
 
 class Bookmark(Base):
+    "Entries for sites we want to keep track of."
     __tablename__ = 'bookmarks'
 
     id = Column(Integer, primary_key=True)
@@ -24,7 +36,9 @@ class Bookmark(Base):
     def __repr__(self):
         return "<Bookmark named %s>" % self.name
 
+
 class Tag(Base):
+    "Tags applied to bookmarks to organize them."
     __tablename__ = 'tags'
 
     id = Column(Integer, primary_key=True)
