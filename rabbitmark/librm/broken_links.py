9b45b31a1cbf3649ac5b4433ab42005ca5c5c078
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-08-14 23:05:21 -0500

    spoof user agent when checking for link rot

--- rabbitmark/librm/broken_links.py
+++ rabbitmark/librm/broken_links.py
@@ -37,14 +37,34 @@ class LinkCheck:
             return f"[FAIL] [ERR] {self.error_description}: {self.name} ({self.url})"
 
 
+def _get_user_agent():
+    """
+    When checking for link rot, some websites will return a 403 if we are
+    honest and say we're Python's requests library, which makes the list
+    inaccurate. To avoid this, we can pretend to be a real browser. The
+    browser we're pretending to be can be adjusted with this string.
+
+    See <https://en.wikipedia.org/wiki/User_agent#User_agent_spoofing>. This
+    is a very simple way of bypassing the checks and can easily be detected
+    by an attentive server; our intent is not to get into a web-scraping war
+    with anyone!
+    """
+    return ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/83.0.4103.97 Safari/537.36')
+
+
 # pylint: disable=too-many-return-statements
 def _check(pk: int, name: str, url: str) -> LinkCheck:
     """
     Given the url /url/, check to see if it accessible, and return a LinkCheck
     object with the URL, primary key, and name, as well as the results of the check.
     """
+    headers = {
+        'User-Agent': _get_user_agent()
+    }
     try:
-        r = requests.head(url, timeout=10, allow_redirects=True)
+        r = requests.head(url, timeout=10, allow_redirects=True, headers=headers)
     except requests.exceptions.SSLError:
         return LinkCheck(pk, name, url, None, "Invalid SSL certificate")
     except requests.exceptions.ConnectionError:
@@ -72,7 +92,7 @@ def scan(session, callback: Callable[[int, int, LinkCheck], None],
     If /only_failures/ is set, only items which have failed will trigger a
     callback. The items with no issues will never be returned to the caller.
     """
-    marks = session.query(Bookmark).all()[0:200]
+    marks = session.query(Bookmark).all()
 
     futures = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
