ad403a155b834f8bc856dbc019f1614944ce8d2b
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-08-14 22:50:57 -0500

    set link-scan timeout to 10 seconds
    The default appears to be a great deal longer (maybe "until the network
    layer gives up", and can cause the scan to lock up for a while at the
    end.)

--- rabbitmark/librm/broken_links.py
+++ rabbitmark/librm/broken_links.py
@@ -44,7 +44,7 @@ def _check(pk: int, name: str, url: str) -> LinkCheck:
     object with the URL, primary key, and name, as well as the results of the check.
     """
     try:
-        r = requests.get(url)
+        r = requests.get(url, timeout=10)
     except requests.exceptions.SSLError:
         return LinkCheck(pk, name, url, None, "Invalid SSL certificate")
     except requests.exceptions.ConnectionError:
