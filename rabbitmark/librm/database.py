9ec4abd452ba768fd6cd1e02af9136a74246739c
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-10-03 14:14:03 -0500

    don't hard-code database location
    It will now go in an appdata location. If this doesn't work, or for
    purposes like testing, an environment variable can be set to choose a
    new location.

--- rabbitmark/librm/database.py
+++ rabbitmark/librm/database.py
@@ -2,6 +2,10 @@
 database.py - general database management
 """
 
+import os
+from pathlib import Path
+import platform
+
 # pylint: disable=no-name-in-module
 from sqlalchemy import create_engine, event
 from sqlalchemy.engine import Engine
@@ -22,9 +26,31 @@ def set_sqlite_pragma(dbapi_connection, connection_record) -> None:
     cursor.close()
 
 
+def _get_datadir() -> Path:
+    this_os = platform.system()
+    if this_os == 'Windows':
+        path = Path(os.path.expandvars("%APPDATA%\\RabbitMark"))
+    elif this_os == 'Darwin':
+        path = Path(os.path.expanduser("~/Library/RabbitMark"))
+    elif this_os == 'Linux':
+        xdg_home = os.environ.get('XDG_DATA_HOME', str(Path.home() / ".local"))
+        path = Path(xdg_home) / "share" / "RabbitMark"
+    else:
+        # try to fall back, but no guarantees at this point...
+        path = Path(os.path.expanduser("~/.rabbitmark"))
+    return path
+
+
 def make_Session() -> SessionType:
     "Create a SQLAlchemy Session object, from which sessions can be spawned."
-    engine = create_engine('sqlite:///sorenmarks-test.db')
+    path_from_env = os.environ.get("RABBITMARK_DATABASE", None)
+    if path_from_env:
+        database_path = path_from_env
+    else:
+        database_path = str(_get_datadir())
+    sqlite_uri = f"sqlite:///{database_path}/rabbitmark.db"
+
+    engine = create_engine(sqlite_uri)
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine) # will not recreate existing tables/dbs
     return Session
